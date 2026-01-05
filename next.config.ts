48da8cccd034ed05f498aa35eec26c0b615f7dc2
Carlos Florêncio <carlosmflorencio@gmail.com>
2026-01-05 22:58:08 +0000

    feat: github deploy workflows

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/mathpad",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
 }
 
 export default nextConfig
